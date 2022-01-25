4f06731ce8d01cda209ce48d6a26be5e2167610f
alexAgnus <53832287+alexAgnus@users.noreply.github.com>
2022-01-25 14:47:26 -0600

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -17,7 +17,15 @@ server.app.use(body_parser_1.default.json());
 //FileUpload
 server.app.use((0, express_fileupload_1.default)());
 //Configurar Cors
-server.app.use((0, cors_1.default)({ origin: true, credentials: true }));
+//server.app.use((0, cors_1.default)({ origin: true, credentials: true }));
+server.app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    //res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    //res.header("Access-Control-Allow-Headers", 'Origin, X-Requested-With , Content-Type , Accept , application/json ,token');
+    res.header("Access-Control-Allow-Headers", '*');
+    next();
+});
 //Rutas de mi app
 server.app.use('/user', usuario_1.default);
 server.app.use('/posts', post_1.default);
