0a4075b5e9a5c811c2ff87456b08bff0d0991515
alexAgnus <53832287+alexAgnus@users.noreply.github.com>
2022-01-25 13:23:40 -0600

    Update index.ts

--- index.ts
+++ index.ts
@@ -18,7 +18,16 @@ server.app.use(bodyParser.json());
 server.app.use(fileUpload());
 
 //Configurar Cors
-server.app.use(cors({ origin:true, credentials: true }));
+//server.app.use(cors({ origin:true, credentials: true }));
+
+server.app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    //res.header("Access-Control-Allow-Headers", 'Origin, X-Requested-With , Content-Type , Accept , application/json ,token');
+    res.header("Access-Control-Allow-Headers", '*');
+    next();
+});
 
 
 //Rutas de mi app
